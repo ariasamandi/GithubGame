c2fd58243cc3ecbb355b5f3342acd59c8b443bce
Aria Samandi <ariasamandi@gmail.com>
2024-05-05 21:22:21 -0700

    Initial Commit

---
+++ GithubGame/GithubGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubGameApp.swift
+//  GithubGame
+//
+//  Created by patron on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
